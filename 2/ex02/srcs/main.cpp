363eb753f39c7cb25509666948b59a4b412e2c0d
William Etievent Cazorla <wetieven@student.42lyon.fr>
2022-03-22 17:20:03 +0100

    2/ex02 more legible tester

--- 2/ex02/srcs/main.cpp
+++ 2/ex02/srcs/main.cpp
@@ -8,14 +8,28 @@ int main( void ) {
 	Fixed a;
 	Fixed const b(Fixed(5.05f) * Fixed(2));
 
+	std::cout << "\"std::cout << a << std::endl;\"" << std::endl;
 	std::cout << a << std::endl;
+	std::cout << "Prefix incrementation" << std::endl;
+	std::cout << "---------------------" << std::endl;
+	std::cout << "\"std::cout << ++a << std::endl;\"" << std::endl;
 	std::cout << ++a << std::endl;
+	std::cout << "\"std::cout << a << std::endl;\"" << std::endl;
 	std::cout << a << std::endl;
+	std::cout << "---------------------" << std::endl;
+	std::cout << "---------------------" << std::endl;
+	std::cout << "Postfix incrementation" << std::endl;
+	std::cout << "---------------------" << std::endl;
+	std::cout << "\"std::cout << a++ << std::endl;\"" << std::endl;
 	std::cout << a++ << std::endl;
+	std::cout << "\"std::cout << a << std::endl;\"" << std::endl;
 	std::cout << a << std::endl;
+	std::cout << "---------------------" << std::endl;
 
+	std::cout << "\"std::cout << b << std::endl;\"" << std::endl;
 	std::cout << b << std::endl;
 
+	std::cout << "\"std::cout << Fixed::max(a, b) << std::endl;\"" << std::endl;
 	std::cout << Fixed::max(a, b) << std::endl;
 
 
