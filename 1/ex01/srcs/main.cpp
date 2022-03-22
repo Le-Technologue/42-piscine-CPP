7406e6d4fd0f674fbfe566e83cf017f096b88cc8
William Etievent Cazorla <wetieven@student.42lyon.fr>
2022-03-22 14:01:35 +0100

    1/ex01 stoul conversion unfolded in tester... ...to comply with the catch 22 situation between not using C++11 converters and not using convenient C converters... Now enjoy this disgusting bloated C++98 style stringstream interfaced conversion.

--- 1/ex01/srcs/main.cpp
+++ 1/ex01/srcs/main.cpp
@@ -1,27 +1,32 @@
-#include <cstdlib>
 #include "Zombie.hpp"
+#include <iostream>
+#include <sstream>
 
 int main( int ac, char *av[] )
 {
-	Zombie	*horde;
-	size_t	amount;
+	Zombie				*horde;
+	std::string			amount_str;
+	int					amount_nbr;
 
-	if (ac != 3)
+	if ( ac != 3 || !av[1] )
 	{
 		std::cout << "USAGE: ./test [amount of zombies] [name]" << std::endl;
 		return 0;
 	}
-	amount = strtoul(av[1], NULL, 10);
 
-	if (amount == 0 || amount > 200)
+	amount_str = av[1];
+	std::stringstream	amount_ss(amount_str);
+	amount_ss >> amount_nbr;
+
+	if (amount_nbr <= 0 || amount_nbr > 200)
 	{
 		std::cout << "C'mon, with the funky values." << std::endl;
 		std::cout << "This is a lame exercice to learn the syntax of array allocation in C++, not the OS of a nuclear reactor." << std::endl;
 		return 0;
 	}
 
-	horde = zombieHorde(amount, av[2]);
-	for (size_t i = 0; i < amount; i++)
+	horde = zombieHorde(amount_nbr, av[2]);
+	for (int i = 0; i < amount_nbr; i++)
 		horde[i].announce();
 
 	delete [] horde;
