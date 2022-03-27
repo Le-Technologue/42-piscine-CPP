3cbd75f6cf009f0e42b1575448aeab5106c5d1d4
William Cazorla <wetieven@student.42lyon.fr>
2022-03-27 18:16:13 +0200

    7/ex02 Class template (Array)

---
+++ 7/ex02/incs/Array.hpp
@@ -0,0 +1,62 @@
+#ifndef ARRAY_HPP
+# define ARRAY_HPP
+
+# include <iostream>
+
+template< typename T >
+class Array {
+
+private:
+
+	unsigned int	_size;
+	T *				_data;
+
+public:
+
+	Array() : _size(0)
+	{
+		_data = NULL;
+	}
+
+	Array( unsigned int n ) : _size(n)
+	{
+		_data = new T[n];
+	}
+
+    Array( const Array & src )
+	{
+		*this = src;
+	}
+
+	~Array()
+	{
+		delete [] _data;
+	}
+
+	Array &			operator=( const Array & rhs )
+	{
+		delete [] _data;
+
+		_size = rhs._size;
+		_data = new T[rhs._size];
+
+		for (unsigned int i = 0 ; i < _size; i++)
+			_data[i] = rhs._data[i];
+
+		return *this;
+	}
+
+	T &				operator[]( unsigned int idx )
+	{
+		if ( idx >= _size )
+			throw std::out_of_range("Index is exceeding array range");
+		return _data[idx];
+	}
+
+	unsigned int	size() const
+	{
+		return _size;
+	}
+};
+
+#endif
---
+++ 7/ex02/srcs/main.cpp
@@ -0,0 +1,85 @@
+#include "Array.hpp"
+
+int		main( void ) {
+
+	std::cout << "[ ~~~~~ CONSTRUCTING TWO ARRAYS ~~~~~ ]\n" << std::endl;
+	std::cout << "\"Array<int>	empty;\"" << std::endl;
+	std::cout << "v" << std::endl;
+	Array<int>	empty;
+	std::cout << std::endl;
+
+	std::cout << "\"Array<int>	quad(4);\"" << std::endl;
+	std::cout << "v" << std::endl;
+	Array<int>	quad(4);
+	std::cout << std::endl;
+
+	std::cout << "[ ~~~~~ [] OPERATOR TESTS ~~~~~ ]\n" << std::endl;
+	std::cout << "[ ~~~~~ initializing \"quad\" array ~~~~~ ]\n" << std::endl;
+
+	std::cout << "\"for ( unsigned int i = 0 ; i < quad.size(); i++ )\"" << std::endl;
+	std::cout << "v" << std::endl;
+	for ( unsigned int i = 0 ; i < quad.size(); i++ )
+	{
+		std::cout << "\"quad[i] = i;\"" << std::endl;
+		std::cout << "v" << std::endl;
+		quad[i] = i;
+	}
+
+	std::cout << "\n[ ~~~~~ displaying \"quad\" array ~~~~~ ]\n" << std::endl;
+
+	std::cout << "\"for ( unsigned int i = 0 ; i < quad.size(); i++ )\"" << std::endl;
+	std::cout << "v" << std::endl;
+	for ( unsigned int i = 0 ; i < quad.size(); i++ )
+	{
+		std::cout << "\"std::cout << quad[i] << std::endl;\"" << std::endl;
+		std::cout << "v" << std::endl;
+		std::cout << quad[i] << std::endl;
+	}
+
+	std::cout << "\n[ ~~~~~ OUT OF RANGE EXCEPTION ~~~~~ ]\n" << std::endl;
+	std::cout << "\"try { std::cout << empty[0] << std::endl; }\"" << std::endl;
+	std::cout << "v" << std::endl;
+	try { std::cout << empty[0] << std::endl; }
+	
+	catch ( std::exception & e ) {
+		std::cout << "\"catch ( std::exception & e ) {\"" << std::endl;
+		std::cout << "v" << std::endl;
+		std::cout << e.what() << ". Do you want to segfault you crazy thing?\n";
+		std::cout << std::endl;
+	}
+
+	std::cout << "\n[ ~~~~~ COPY CONSTRUCTION ~~~~~ ]\n" << std::endl;
+	std::cout << "\"empty = quad;\"" << std::endl;
+	std::cout << "v" << std::endl;
+	empty = quad;
+	std::cout << std::endl;
+
+	std::cout << "\n[ ~~~~~ PROVING DEEP COPY ~~~~~ ]\n" << std::endl;
+	std::cout << "\"quad[0] = 666;\"" << std::endl;
+	std::cout << "v" << std::endl;
+	quad[0] = 666;
+	std::cout << std::endl;
+
+	std::cout << "\n[ ~~~~~ displaying (not) \"empty\" array ~~~~~ ]\n" <<
+	std::endl;
+	std::cout << "\"for ( unsigned int i = 0 ; i < empty.size(); i++ )\"" << std::endl;
+	std::cout << "v" << std::endl;
+	for ( unsigned int i = 0 ; i < empty.size(); i++ )
+	{
+		std::cout << "\"empty[i] = i;\"" << std::endl;
+		std::cout << "v" << std::endl;
+		std::cout << empty[i] << std::endl;
+	}
+
+	std::cout << "\n[ ~~~~~ displaying \"quad\" array ~~~~~ ]\n" << std::endl;
+	std::cout << "\"for ( unsigned int i = 0 ; i < quad.size(); i++ )\"" << std::endl;
+	std::cout << "v" << std::endl;
+	for ( unsigned int i = 0 ; i < quad.size(); i++ )
+	{
+		std::cout << "\"quad[i] = i;\"" << std::endl;
+		std::cout << "v" << std::endl;
+		std::cout << quad[i] << std::endl;
+	}
+
+	return 0;
+}
